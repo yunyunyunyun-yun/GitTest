5681a56803ba6c4f95c0ae854607d8dc799635e3
yunyunyunyun-yun <65709767+yunyunyunyun-yun@users.noreply.github.com>
2021-10-29 10:30:50 +0800

    Update Hello.java
    define number a equals 1

--- src/main/com/yun/Hello.java
+++ src/main/com/yun/Hello.java
@@ -5,8 +5,10 @@ package main.com.yun;
  */
 public class Hello {
     public static void main(String[] args) {
+        int a = 1;
         System.out.println("Hello!");
         System.out.println("Hello!");
-        System.out.println("Hello!");
+        System.out.println(a);
+        
     }
 }
