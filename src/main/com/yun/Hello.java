e0b76d2516d06cbb6257d499f9c4fbddec722050
yun <pyb1127@hotmail.com>
2021-10-29 10:05:24 +0800

    v0.1.0 first commit

---
+++ src/main/com/yun/Hello.java
@@ -0,0 +1,13 @@
+package main.com.yun;
+
+import java.util.ArrayList;
+
+/**
+ * @author yun
+ */
+public class Hello {
+    public static void main(String[] args) {
+        ArrayList<Object> arrayList = new ArrayList<>();
+        System.out.println("Hello!");
+    }
+}
