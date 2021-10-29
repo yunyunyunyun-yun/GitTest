63f79a86b103e02a0394845d836953a3ffe5c9c4
yun <pyb1127@hotmail.com>
2021-10-29 10:17:27 +0800

    v0.1.1 add three lines!

--- src/main/com/yun/Hello.java
+++ src/main/com/yun/Hello.java
@@ -1,12 +1,12 @@
 package main.com.yun;
 
-import java.util.ArrayList;
-
 /**
  * @author yun
  */
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello!");
         System.out.println("Hello!");
     }
 }
